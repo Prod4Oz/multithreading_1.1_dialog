769b06be5ae4752405f507f6928dcd1d62fa8d67
Ivan Smirnov <nn52-ivan@outlook.com>
2022-07-30 00:43:25 -0400

    Task 2 move

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -1,16 +1,41 @@
 package homework;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
+import java.util.Arrays;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Callable<Integer> callable = new MyCallable();
-        FutureTask futureTask = new FutureTask(callable);
-        new Thread(futureTask).start();
-        System.out.println(futureTask.get());
+    public static void main(String[] args) {
+        Callable<Integer> myCallable1 = new MyCallable("Поток 1 ");
+        Callable<Integer> myCallable2 = new MyCallable("Поток 2 ");
+        Callable<Integer> myCallable3 = new MyCallable("Поток 3 ");
+        Callable<Integer> myCallable4 = new MyCallable("Поток 4 ");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        Future<Integer> future1 = executorService.submit(myCallable1);
+        Future<Integer> future2 = executorService.submit(myCallable2);
+        Future<Integer> future3 = executorService.submit(myCallable3);
+        Future<Integer> future4 = executorService.submit(myCallable4);
+
+        try {
+            System.out.println(myCallable1 + "workTask ->" + future1.get());
+            System.out.println(myCallable2 + "workTask ->" + future2.get());
+            System.out.println(myCallable3 + "workTask ->" + future3.get());
+            System.out.println(myCallable4 + "workTask ->" + future4.get());
+
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        try {
+            Integer result = executorService.invokeAny(Arrays.asList(myCallable1, myCallable2, myCallable3, myCallable4));
+            System.out.println("Result" + result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Finish");
+        executorService.shutdown();
+
 
     }
 }
--- src/homework/MyCallable.java
+++ src/homework/MyCallable.java
@@ -3,15 +3,26 @@ package homework;
 import java.util.concurrent.Callable;
 
 public class MyCallable implements Callable<Integer> {
+    private String name;
 
+    public MyCallable(String name) {
+        this.name = name;
+    }
 
     @Override
     public Integer call() throws Exception {
+        Thread.currentThread().setName(name);
         int count = 0;
-        for (int i = 1; i <=10; i++, count++){
-            Thread.sleep(200);
+        for (int i = 1; i <=4; i++, count++){
+            Thread.sleep(3000);
+            System.out.println("Hi ! " + Thread.currentThread().getName() + " - is work");
         }
 
         return count;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
